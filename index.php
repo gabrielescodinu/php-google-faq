b8fab9a3d37eb4bd84b45058222da7cf4f1d1eeb
gabriele <gabrielescodinu96@gmail.com>
2021-02-01 15:20:55 +0100

    creating faq

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+    $database = [
+        'answer' => [
+            "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+            "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+        ],
+        'reply' => [
+            "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+            "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
+        ],
+    ];
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php for ($i=0; $i < count($database); $i++) { 
+        $data = $database[$i]; ?>
+        <?php echo $data['answer']; ?>
+        <?php echo $data['reply']; ?>
+    <?php } ?>
+    
+</body>
+</html>
